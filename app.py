08aad5672f48e68e1f165a552e4832e75d2dddab
YannMSFT <YannMSFT@users.noreply.github.com>
2021-01-31 23:11:33 +0100

    bugfix

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html', title="page", jsonfile=json.dumps(data))
+    return render_template('index.html', title="page")
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
