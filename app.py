11a11200e9c3f7a7b0fc08c051c8b37dcbbf8b73
YannMSFT <30301867+YannMSFT@users.noreply.github.com>
2021-03-08 22:34:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask, render_template, abort, url_for, json, jsonify
+from flask import request
 import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html', title="page")
+    useragent = request.headers.get('User-Agent')
+    return render_template('index.html', title="page", myuseragent=useragent)
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
